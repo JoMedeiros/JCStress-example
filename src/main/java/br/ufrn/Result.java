93fa01c0917bdd7c495fa0e7771ea7ef62054afb
Josivan Medeiros da Silva Gois <josivanmedeiros@A307-04L.imd.local>
2019-09-23 18:27:21 -0300

    fork-join

--- src/main/java/br/ufrn/Result.java
+++ src/main/java/br/ufrn/Result.java
@@ -6,16 +6,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Result {
     //private Hashtable<String, Integer> totais = new Hashtable<>(); // Número de provas analisadas
-    private  Hashtable<String, AtomicInteger> totais = new Hashtable<String, AtomicInteger>(); // Número de provas analisadas
+    private Hashtable<String, AtomicInteger> totais = new Hashtable<String, AtomicInteger>(); // Número de provas analisadas
     private Hashtable<String, AtomicInteger[]> acertos = new Hashtable<String, AtomicInteger[]>(); // Totais de acerto por questão
     private String [] provas = {"CN","CH","LC","MT"};
     private String [] provasExtenso = {"Ciências da Natureza","Ciências Humanas","Linguagens e Códigos","Matemática"};
     private boolean finished = false;
     private final Semaphore available = new Semaphore(1,true);
-
-    public Hashtable<String, AtomicInteger[]> getAcertos() {
-        return acertos;
-    }
 
     public Result(){
         initialize();
@@ -29,6 +25,15 @@ public class Result {
             totais.put(prova, new AtomicInteger(0));
         }
     }
+
+    /**
+     * A Class to use in jcstress test
+     * @return
+     */
+    public Hashtable<String, AtomicInteger[]> getAcertos() {
+        return acertos;
+    }
+
     /**
      * @brief Incrementa o contador de acertos na prova.
      * @param respostas Um vetor que representa os acertos de um aluno. Cada valor representa acerto com 1 e erro com 0.
@@ -46,6 +51,15 @@ public class Result {
         //totais.put(prova, totais.get(prova).incrementAndGet());
         totais.get(prova).incrementAndGet();
         //available.release();
+    }
+
+    public void sum(Result result){
+        for (String pv : provas) {
+            for (int i = 0; i < this.acertos.get(pv).length; i++) {
+                this.acertos.get(pv)[i].addAndGet(result.acertos.get(pv)[i].get());
+            }
+            this.totais.get(pv).addAndGet(result.totais.get(pv).get());
+        }
     }
 
     /**
